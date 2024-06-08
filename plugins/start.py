876a2183b64890bc7be73b50261a8c33cb0c8c11
YuuichiSama <142111069+YuuichiSama@users.noreply.github.com>
2024-06-08 19:45:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -122,11 +122,11 @@ REPLY_ERROR = "<code>Use this command as a reply to any telegram message without
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ⚡️", url=client.invitelink2),
-            InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ⚡️", url=client.invitelink3),
+            InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 1⚡️", url=client.invitelink2),
+            InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 2⚡️", url=client.invitelink3),
         ],
         [
-            InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴀɴɪᴍᴇ ᴄʜᴀᴛ ɢʀᴏᴜᴘ⚡️", url=client.invitelink),
+            InlineKeyboardButton(text="⚡️ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ 3⚡️", url=client.invitelink),
         ]
     ]
     try:
